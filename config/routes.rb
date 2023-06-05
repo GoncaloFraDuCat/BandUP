11891a0fe660f4331bc490ce43dfe9b31c15909c
goncalo catarro <goncalocatarro@gmail.com>
2023-06-05 16:35:12 +0100

    day1

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: "home#index"
-  resources :user
+  root to: "pages#home"
+  resources :users, only: [:index, :edit, :show]
 end
