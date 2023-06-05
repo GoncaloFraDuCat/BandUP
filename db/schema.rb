3dfb0662abe4b23803b556fd692119166c2c7e69
luis henrique <luisham2001@gmail.com>
2023-06-05 15:20:55 +0100

    user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_05_140809) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_05_141530) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -19,6 +19,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_05_140809) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "city"
+    t.integer "age"
+    t.string "instruments"
+    t.string "interests"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
