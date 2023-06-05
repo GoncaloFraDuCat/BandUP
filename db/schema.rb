a4fc2ee641c9ff3d06a208103784de14a216af47
luis henrique <luisham2001@gmail.com>
2023-06-05 15:35:07 +0100

    models and their conections

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_05_141530) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_05_142917) do
+  create_table "projects", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "interests"
+    t.date "date"
+    t.string "title"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_projects_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -28,4 +39,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_05_141530) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "projects", "users"
 end
