379c8641f0125648df175a89266fd4831e050f44
luis henrique <luisham2001@gmail.com>
2023-06-05 15:46:47 +0100

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, presence: true
+  validates :city, presence: true
+  validates :age, presence: true
+  validates :instruments, presence: true
+  validates :interests, inclusion: { in: %w(Jam Collab Performace), message: "%{value} is not a valid interests" }
+  has_many :projects, dependent: :destroy
 end
