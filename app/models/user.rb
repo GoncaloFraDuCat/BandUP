4f08594be730bad46f2b91888d5276432ec89e99
luis henrique <luisham2001@gmail.com>
2023-06-05 15:52:01 +0100

    projects validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :name, presence: true
+  validates :email, uniqueness: true
+  validates :name, uniqueness: true
   validates :city, presence: true
   validates :age, presence: true
   validates :instruments, presence: true
